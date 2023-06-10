4637736f4d44f76365b5d2437c3596842a26622c
Simon Knittel <hallo@simonknittel.de>
2023-06-10 10:12:26 +0200

    chore: Upgrade files to match T3 App v7.13.1

---
+++ app/tailwind.config.ts
@@ -0,0 +1,28 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      animation: {
+        rgb: "rgb 10s ease infinite",
+      },
+      keyframes: {
+        rgb: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
