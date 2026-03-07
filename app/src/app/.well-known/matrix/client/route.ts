77f53faa986b38bf1260ff90f779b4e67c61489b
Simon Knittel <hallo@simonknittel.de>
2026-03-07 14:22:28 +0100

    fix(Synapse): use correct url format

--- app/src/app/.well-known/matrix/client/route.ts
+++ app/src/app/.well-known/matrix/client/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export function GET() {
   return NextResponse.json({
-    "m.homeserver": "synapse.simonknittel.de:443",
+    "m.homeserver": "https://synapse.simonknittel.de",
   });
 }
